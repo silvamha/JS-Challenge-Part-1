b3bcc39dfc43dec5ddb1f2dcb95a8e7a2e2b561a
Ed <silva.email@yahoo.com>
2024-09-01 05:46:46 -0500

    Linked HTML Doc

---
+++ index.js
@@ -0,0 +1,10 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+let fistName = "Edson";
+let lastname = "Silva";
+
+console.log(`${fistName} ${lastname}`);
